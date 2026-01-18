d06244cb9b483910c81202230f453c828248b2ce
Emma24968 <emnabs12@gmail.com>
2026-01-18 23:35:22 +0100

    Centered the nav container

--- react-typescript-demo/src/components/navigation/Header.tsx
+++ react-typescript-demo/src/components/navigation/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <div>
-      <h1 className="font-[var(--font-times)] text-[30px] font-normal ">
+      <h1 className="font-[var(--font-times)] text-[30px] font-normal text-white ">
         NEWFRONT
       </h1>
     </div>
--- react-typescript-demo/src/components/navigation/Navigation.tsx
+++ react-typescript-demo/src/components/navigation/Navigation.tsx
@@ -1,18 +1,20 @@
-import { Header } from "./Header"
+import { Header } from "./Header";
 
 export const Navigation = () => {
   return (
-    <div className="bg-[#202C2F] ">
-        <Header />
+    <div className="bg-[#202C2F] w-[87%] container mx-auto flex  gap-35 ">
+      <Header />
+      <div  className="flex gap-10 ml-20">
         <ul>Business Insurance</ul>
         <ul>Total Rewards</ul>
         <ul>Technology</ul>
         <ul>About</ul>
         <ul>Insights</ul>
-        <div className="div">
-            <ul>Login</ul>
-            <button>Talk to an Expert</button>
-        </div>
+      </div>
+      <div className="flex gap-8">
+        <ul>Login</ul>
+        <button>Talk to an Expert</button>
+      </div>
     </div>
   );
 };
