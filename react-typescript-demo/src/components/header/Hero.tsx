73b90b2622bfad7655eaff84cb98004a7205178e
Emma24968 <emnabs12@gmail.com>
2026-01-19 14:56:37 +0100

    Created the hero component

---
+++ react-typescript-demo/src/components/header/Hero.tsx
@@ -0,0 +1,14 @@
+export const Hero = () => {
+  return (
+    <div className="div">
+      <div className="div"></div>
+      <div className="div">
+        <h3>De-Risking Human Progress</h3>
+        <p>
+          With elite insurance expertise empowered by breakthrough technology,
+          Newfront is the modern insurance brokerage for the 21st century.
+        </p>
+      </div>
+    </div>
+  );
+};
