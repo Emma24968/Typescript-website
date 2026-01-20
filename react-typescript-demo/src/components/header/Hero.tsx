eff6320ffd8adc5cc37de4f1469fbecaeaee3ca7
Emma24968 <emnabs12@gmail.com>
2026-01-20 03:15:50 +0100

    Added background image to the hero section

--- react-typescript-demo/src/components/header/Hero.tsx
+++ react-typescript-demo/src/components/header/Hero.tsx
@@ -1,10 +1,13 @@
+import colleagueimg from "../../assets/colleague.webp";
 export const Hero = () => {
   return (
-    <div className="flex">
-      <div className="bg-[url(.../assets/)]
-"></div>
-      <div className="div">
-        <h3>De-Risking Human Progress</h3>
+    <div className="flex  ">
+      <section
+        className="h-screen bg-cover w-[500px] bg-center"
+        style={{ backgroundImage: `url(${colleagueimg})` }}
+      ></section>{" "}
+      <div className="w-[50%]">
+        <h3 className="">De-Risking Human Progress</h3>
         <p>
           With elite insurance expertise empowered by breakthrough technology,
           Newfront is the modern insurance brokerage for the 21st century.
