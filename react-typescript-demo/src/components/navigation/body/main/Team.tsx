52efa30bd7f73e3bf46d9f727f305dbf4593e4c9
Emma24968 <emnabs12@gmail.com>
2026-01-22 18:12:43 +0100

    Installed lucide icons

--- react-typescript-demo/src/components/navigation/body/main/Team.tsx
+++ react-typescript-demo/src/components/navigation/body/main/Team.tsx
@@ -3,11 +3,11 @@ import team from "../../../../assets/TalentxTech.webp";
 
 export const Team = () => {
   return (
-    <div>
-      <img src={team} alt="" />
-      <div>
-        <div className="">Empowered Expertise</div>
-        <div className="">Talent Meets Tech</div>
+    <div className="mt-[5rem] flex">
+      <img src={team} alt="" className="w-[30rem]  rounded-3xl" />
+      <div className="w-[30%] ">
+        <div className="font-semibold text-[13px] font-[aeronik,sans serif]">Empowered Expertise</div>
+        <div className="font-[aeronik,sans serif] text-[40px] ">Talent Meets Tech</div>
         <div className="">
           Technology should enhance human expertise, not replace it. By
           equipping our elite insurance talent with tech, we're creating a
