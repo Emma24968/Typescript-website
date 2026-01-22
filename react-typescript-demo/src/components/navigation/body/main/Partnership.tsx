9ebd9080410321d26e3d946d11c66e0d160a741e
Emma24968 <emnabs12@gmail.com>
2026-01-22 18:02:34 +0100

    Created the team component and added the image

--- react-typescript-demo/src/components/navigation/body/main/Partnership.tsx
+++ react-typescript-demo/src/components/navigation/body/main/Partnership.tsx
@@ -1,13 +1,16 @@
 import { ImageSlide } from "./Carousel/ImageSlide";
 import { Slider } from "./Slider";
+import { Team } from "./Team";
 import { Testimonial } from "./Testimonial";
 
 export const Partnership = () => {
   return (
-    <div className="bg-white rounded-2xl h-[50rem] p-8">
+    <div className="bg-white rounded-2xl h-[70rem] p-8">
       <Slider />
       <Testimonial />
       <ImageSlide />
+      <Slider />
+      <Team />
     </div>
   );
 };
---
+++ react-typescript-demo/src/components/navigation/body/main/Team.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import team from "../../../../assets/TalentxTech.webp";
+
+export const Team = () => {
+  return (
+    <div>
+      <img src={team} alt="" />
+      <div>
+        <div className="">Empowered Expertise</div>
+        <div className="">Talent Meets Tech</div>
+        <div className="">
+          Technology should enhance human expertise, not replace it. By
+          equipping our elite insurance talent with tech, we're creating a
+          competitive edge for our people and clients. For example:
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
+};
