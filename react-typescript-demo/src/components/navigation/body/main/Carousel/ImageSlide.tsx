1e20af5859248531e86f6ed80a0ab39779b644b6
Emma24968 <emnabs12@gmail.com>
2026-01-22 14:02:38 +0100

    Imported and created an array for the slider images

---
+++ react-typescript-demo/src/components/navigation/body/main/Carousel/ImageSlide.tsx
@@ -0,0 +1,18 @@
+import Beone from '../../../../../assets/BeOne.webp'
+import Astranis from '../../../../../assets/Client_logo_astranis.webp'
+import Conversica from '../../../../../assets/Conversica.webp'
+import Partners from '../../../../../assets/DBL_Partners.webp'
+import Capital from '../../../../../assets/Foundation_Capital.webp'
+import Lime from '../../../../../assets/logo_home_lime.png'
+import Thistle from '../../../../../assets/logo_home_thistle.png'
+import Ycombinator from '../../../../../assets/logo_home_y-combinator.png'
+import Calm from '../../../../../assets/logo_home_calm.png'
+
+export const ImageSlide=()=>{
+    const slides=[Calm,Ycombinator,Thistle,Lime,Partners,Astranis,Beone,Capital,Conversica]
+    return(
+<div>
+    
+</div>
+    )
+}
