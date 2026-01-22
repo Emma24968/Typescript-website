13e9fb39ad7cedcec76682eb6cbe47a747621b6f
Emma24968 <emnabs12@gmail.com>
2026-01-22 02:15:50 +0100

    Ceeated the image slider

--- react-typescript-demo/src/components/navigation/body/main/Partnership.tsx
+++ react-typescript-demo/src/components/navigation/body/main/Partnership.tsx
@@ -1,9 +1,11 @@
 import { Slider } from "./Slider";
+import { Testimonial } from "./Testimonial";
 
 export const Partnership = () => {
   return (
     <div className="bg-white rounded-2xl h-[50rem] p-8">
       <Slider />
+      <Testimonial />
     </div>
   );
 };
---
+++ react-typescript-demo/src/components/navigation/body/main/Testimonial.tsx
@@ -0,0 +1,12 @@
+export const Testimonial = () => {
+  return (
+    <div className="flex items-center mt-[5rem]">
+      <h3 className="font-[aeronik,sans serif] w-[60%] text-[40px]">Clients Trust Newfront</h3>
+      <p className="text-[20px]">
+        With hundreds of veteran brokers and dedicated<br />  service professionals, we
+        combine domain <br /> expertise with data-driven market intelligence to <br /> support
+        our clients.
+      </p>
+    </div>
+  );
+};
